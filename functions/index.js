5f2d7127f26fa74f7c7efe71a9648f79eb6d1244
Spazcool <douglas.wrightiii@gmail.com>
2020-08-23 20:46:34 -0400

    tweak a setting for firebase

--- functions/index.js
+++ functions/index.js
@@ -3,34 +3,34 @@ const app = express();
 const cors = require('cors');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin'); //a hack that allows us to bypass some auth stuff, todo lookup how to avoid using it
-var serviceAccount = require("../key.json"); //used for interacting with DB during local dev
+// const serviceAccount = require("../key.json"); //used for interacting with DB during local dev
 
 // ------------------------ 3 WAYS TO INITIALIZE APP ------------------------
-const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ispyrefi.firebaseio.com"
-});
-// admin.initializeApp();
+// const firebaseApp = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://ispyrefi.firebaseio.com"
+// });
+admin.initializeApp();
 // const firebaseApp = admin.initializeApp(functions.config().firebase);
 
 // ------------------------ EXAMPLE INTERACTION WITH DB ------------------------
-function getDBItems(){
-  const ref = firebaseApp.firestore().collection('houses').orderBy('value', 'desc');
-  return ref.get();
+// function getDBItems(){
+//   const ref = firebaseApp.firestore().collection('houses').orderBy('value', 'desc');
+//   return ref.get();
 // console.log(ref)
   // return ref.once('value').then(snap => snap.val())
  
   // const housesList = db.collection('houses');
   // const myHouse = db.collection('houses').doc('JWb8oyGegTY1HCi9XcaX');
   // const query = housesList.where('value', '>', 100000);
-}
+// }
 
 // ------------------------ EXAMPLE MODIFYING DB ------------------------
 // exports.sendMessage = functions.firestore
 //   .document('houses/{houseID}')
 //   .onCreate((snap, context) => {
 //     const original = snap.data().original;
-//     // const value = snap.data().original.value;
+//     const value = snap.data().original.value;
 //     functions.logger.log('sending message', context.params.houseID, context);
 //     return snap.ref.update({message: `Yolo! called from cloud function`}, {merge: true});
 //   })
@@ -72,7 +72,7 @@ app.get('/timestamp-cached', (req, res) => {
 // app.get('/', (req, res) => res.json('what what!'));
 
 // Expose Express API as a single Cloud Function:
-exports.app = functions.https.onRequest(app);
+exports.thing = functions.https.onRequest(app);
 
 // ------------------------ EXAMPLE URL of deployed cloud function ------------------------
 // FYI this there is a rule in firebase.json file to redirect this URL to our home URL of https://ispyrefi.com:
