3d31b5b0e271f5ca17d2512c2c9029960d91e85e
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-27 20:30:27 -0400

    doing htis again

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React, { useContext } from "react";
-import { Route, Switch, BrowserRouter as Router, Redirect } from "react-router-dom";
-import { AuthProvider, AuthContext } from "./providers/AuthProvider";
-import Home from "./pages/Home";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Houses from "./pages/Houses";
+import React, { useContext } from 'react';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+  Redirect,
+} from 'react-router-dom';
+import { AuthProvider, AuthContext } from './providers/AuthProvider';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Houses from './pages/Houses';
 
 function App() {
   // Here we subscribe the authentication context using the useContext hook
@@ -16,37 +21,41 @@ function App() {
   //   console.log(user)
   // }
 
-  // here we are ceating a private route wrapper to prevent front end routing to 
+  // here we are ceating a private route wrapper to prevent front end routing to
   // restricted pages.  The ({ component: Component, ...rest })  argument that is
-  // passed to this functional component is essentially the same as just passing 
-  // props, but using object destucturing.  the ...rest is literally the rest of 
-  // the props that were not destructured. 
+  // passed to this functional component is essentially the same as just passing
+  // props, but using object destucturing.  the ...rest is literally the rest of
+  // the props that were not destructured.
   const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
       {...rest}
-      render={props =>
-        isAuth ? <Component {...props} /> : <Redirect to="/login" />
+      render={(props) =>
+        isAuth ? <Component {...props} /> : <Redirect to='/login' />
       }
     />
   );
-//todo displayname, if desirable, we'll need to make a call to the db to grab the user names for users that signed up with email
+  //todo displayname, if desirable, we'll need to make a call to the db to grab the user names for users that signed up with email
   return (
     <Router>
       <>
-      <div>Sexy header to go here: {isAuth ? 
-      user.user.displayName ?
-      user.user.displayName : user.user.email
-      : ''}</div>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={props => <Home {...props} />}
-        />
-        <Route exact path="/login" render={props => <Login {...props} />} />
-        <Route exact path="/signup" render={props => <Signup {...props} />} />
-        <PrivateRoute exact path="/houses" component={Houses} />
-      </Switch>
+        <div>
+          Sexy header to go here:{' '}
+          {isAuth
+            ? user.user.displayName
+              ? user.user.displayName
+              : user.user.email
+            : ''}
+        </div>
+        <Switch>
+          <Route exact path='/' render={(props) => <Home {...props} />} />
+          <Route exact path='/login' render={(props) => <Login {...props} />} />
+          <Route
+            exact
+            path='/signup'
+            render={(props) => <Signup {...props} />}
+          />
+          <PrivateRoute exact path='/houses' component={Houses} />
+        </Switch>
       </>
     </Router>
   );
