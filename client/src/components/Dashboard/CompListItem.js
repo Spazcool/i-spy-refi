21b614046100f85659f686518c11602fa3210e00
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-08-30 13:54:58 -0400

    committing before i break shit

--- client/src/components/Dashboard/CompListItem.js
+++ client/src/components/Dashboard/CompListItem.js
@@ -41,37 +41,36 @@ function SimilarProperties() {
     // console.log(data.comparables.zip);
     let { comparables } = data;
     console.log({ comparables });
-    const comparibles = comparables.map((item) => (
-      <li key={item.zip}>{item.address.street}</li>
+    const comparison = comparables.map((item) => (
+      // <li key={item.zip}>{item.address.street}</li>
+      <Card>
+        <CardActionArea>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom></Typography>
+            <Typography variant='h5' component='h2'>
+              {item.address.street}
+            </Typography>
+            <Typography variant='h5' component='h2'>
+              $ {item.lastSoldPrice.value}
+            </Typography>
+            <Typography color='textSecondary'>
+              {item.address.city}, {item.address.state},
+            </Typography>
+            <Typography variant='body2' component='p'>
+              Last sold {item.lastSoldDate}
+            </Typography>
+            <Typography variant='body2' component='p'>
+              Description/Details
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size='small'>Button</Button>
+        </CardActions>
+      </Card>
     ));
-    return comparibles;
+    return comparison;
 
-    // <Card>
-    //   <CardActionArea>
-    //     <CardContent>
-    //       <Typography color='textSecondary' gutterBottom></Typography>
-    //       <Typography variant='h5' component='h2'>
-    //         {data.comparables[0].address.street}
-    //       </Typography>
-    //       <Typography variant='h5' component='h2'>
-    //         $ {data.comparables[0].lastSoldPrice.value}
-    //       </Typography>
-    //       <Typography color='textSecondary'>
-    //         {data.comparables[0].address.city},{' '}
-    //         {data.comparables[0].address.state},
-    //       </Typography>
-    //       <Typography variant='body2' component='p'>
-    //         Last sold {data.comparables[0].lastSoldDate}
-    //       </Typography>
-    //       <Typography variant='body2' component='p'>
-    //         Description/Details
-    //       </Typography>
-    //     </CardContent>
-    //   </CardActionArea>
-    //   <CardActions>
-    //     <Button size='small'>Button</Button>
-    //   </CardActions>
-    // </Card>
     // returns all data
   }
   return <></>;
