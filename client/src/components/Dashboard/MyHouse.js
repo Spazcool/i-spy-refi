58e73b469264fe631eff24b31c8fbead86dcfe07
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-30 23:32:09 -0400

    changes to imageurl retreival

--- client/src/components/Dashboard/MyHouse.js
+++ client/src/components/Dashboard/MyHouse.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 
@@ -11,10 +12,9 @@ import Paper from '@material-ui/core/Paper';
 
 // House Display Info
 
-import axios from 'axios';
 import { DB } from '../../api/firestore';
 import { AuthContext } from '../../providers/AuthProvider';
-
+import { zillow } from '../../api/zillow';
 export default function MyHouse(props) {
   // House Display Info Logic
 
@@ -24,8 +24,6 @@ export default function MyHouse(props) {
   const [citydisplay, setcitydisplay] = useState('');
 
   // House EVAL
-
-  const zillowpropid = '95354572';
   const finishedSqFt = '2466';
   let avgSqFt = 0;
   let avgPerSqFt = 0;
@@ -47,123 +45,28 @@ export default function MyHouse(props) {
       state,
       zip,
     };
+    /////////////////// FIRST API CALL /////////////////
 
+    const displayaddress = await zillow.getaddress(data);
+
+    console.log('houseinfo from zillow :', displayaddress[0].zpid);
     // HardCoded DATA
-    const statedb = await data.state;
-    const citydb = await data.city;
-    setcitydisplay(citydb);
-    const streetdb = await data.street;
-    setstreetdisplay(streetdb);
-    const zipdb = await data.zip;
-
-    // const state = 'NH';
+    const statezillow = displayaddress[0].address.state;
+    const cityzillow = displayaddress[0].address.city;
+    setcitydisplay(cityzillow);
+    const streetzillow = displayaddress[0].address.street;
+    setstreetdisplay(streetzillow);
+    const zillowzpid = displayaddress[0].zpid;
+    
+      // const state = 'NH';
     // const city = 'portsmouth';
     // const street = '31 Sudbury St';
     // const zip = '03801';
     // setTimeout(() => {
-    await axios({
-      method: 'GET',
-      url: 'https://zillow-com.p.rapidapi.com/search/address',
-      headers: {
-        'content-type': 'application/octet-stream',
-        'x-rapidapi-host': 'zillow-com.p.rapidapi.com',
-        'x-rapidapi-key': '26d05b2092msh8d14d2474ce38e0p120b64jsn0baeb38641f3',
-        useQueryString: true,
-      },
-      params: {
-        address: `${streetdb}`,
-        citystatezip: `${citydb} ${statedb} ${zipdb}`,
-      },
-    })
-      .then((response) => {
-        // 2 console.log(
-        //   'streetcitystatezip:',
-        //   streetdb,
-        //   '||',
-        //   citydb,
-        //   statedb,
-        //   zipdb
-        // );
+    //////////////////////// SECOND CALL ///////////////////
 
-        // 3 console.log('res', response.data);
-
-        let id = response.data[0].zpid;
-
-        axios({
-          method: 'GET',
-          url: `https://zillow-com.p.rapidapi.com/property/${id}/media`,
-          headers: {
-            'content-type': 'application/octet-stream',
-            'x-rapidapi-host': 'zillow-com.p.rapidapi.com',
-            'x-rapidapi-key':
-              '26d05b2092msh8d14d2474ce38e0p120b64jsn0baeb38641f3',
-            useQueryString: true,
-          },
-        })
-          .then((response) => {
-            // 4 console.log(
-            //   'RES IMG',
-            //   response.data.imageResults.images[0].highResUrl
-            // );
-            setImage(response.data.imageResults.images[0].highResUrl);
-
-            // TO GET THE HOUSE EVALUTION BASED ON THE AVG SQ FT ////
-            setTimeout(
-              () =>
-                axios({
-                  method: 'GET',
-                  url: `https://zillow-com.p.rapidapi.com/property/${id}/compset`,
-                  headers: {
-                    'content-type': 'application/octet-stream',
-                    'x-rapidapi-host': 'zillow-com.p.rapidapi.com',
-                    'x-rapidapi-key':
-                      '26d05b2092msh8d14d2474ce38e0p120b64jsn0baeb38641f3',
-                    useQueryString: true,
-                  },
-                  params: {
-                    limit: '10',
-                  },
-                })
-                  .then((response) => {
-                    let comlength = response.data.comparables.length;
-                    //console.log('complength' + comlength);
-
-                    // calculating the Average SqFt
-                    let index = 0;
-
-                    for (let i = 0; i < comlength; i++) {
-                      avgSqFt +=
-                        response.data.comparables[i].lastSoldPrice.value /
-                        response.data.comparables[i].finishedSqFt;
-                      index = i + 1;
-                    }
-                    console.log('avgsqft: ', avgSqFt);
-                    avgPerSqFt = avgSqFt / index;
-
-                    console.log('avgpersqft:', avgPerSqFt);
-
-                    // Calculating The House Value
-
-                    settotalHouseValue(finishedSqFt * avgPerSqFt);
-
-                    console.log('housevalue:' + totalHouseValue);
-
-                    // setavgSqFt(result);
-                    // console.log('result:' + avgSqFt);
-                  })
-                  .catch((error) => {
-                    console.log(error);
-                  }),
-              2000
-            );
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const getimageurl = await zillow.getzillowpropid(zillowzpid);
+  
   };
 
   return (
@@ -172,7 +75,8 @@ export default function MyHouse(props) {
         <CardMedia className='media' image={imageData} title='My House' />
         <CardContent>
           <Typography variant='h5' component='h2'>
-            {streetdisplay},{citydisplay}
+            {streetdisplay}
+            {citydisplay}
           </Typography>
           <Typography variant='h5' component='h2'>
             $360,000
