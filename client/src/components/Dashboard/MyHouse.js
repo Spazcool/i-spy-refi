6782b0759803ed7a48166d5fdd7941a8ed15edeb
Spazcool <douglas.wrightiii@gmail.com>
2020-09-02 22:27:03 -0400

    add conditional loading animations to myhouse.js fix some css funkiness

--- client/src/components/Dashboard/MyHouse.js
+++ client/src/components/Dashboard/MyHouse.js
@@ -1,39 +1,85 @@
 import React, { useState, useEffect, useContext } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
-// House Display Info
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+  lazyImage: {
+    width: '100%',
+    'max-height': '150px',
+    alignItems:"center",
+    justifyContent:"center",
+    display: 'flex',
+
+  },
+  block:{
+    display: 'block',
+  }
+}));
 
 export default function MyHouse(props) {
-  //image={imageData}
-  //,{citydisplay},{statedisplay}
-  //  {totalHouseValue}
-  console.log(props.street);
-  console.log(props);
+  const [loaded, setLoaded]= useState(false);
+  const classes = useStyles();
 
-  return (
-    <Paper elevation={4} className='my-house'>
-      <Card className='my-house'>
-        <CardMedia className='media' component='img' title='My House' />
-        <CardContent>
-          <Typography variant='h5' component='h2'>
-            {props.street}
-          </Typography>
-          <Typography variant='h5' component='h2'>
-            {'$'}
-          </Typography>
-          <Typography variant='body2' component='p'>
-            Description/Details
-          </Typography>
-        </CardContent>
+  const checkLoaded = () => {
+    const {imageData, street, description, value } = props;
+    if(imageData && street){ // && description && value (if not all values are sent, the component never loads)
+      setLoaded(true);
+    }
+  }
+  
+  useEffect(() => {
+    checkLoaded();
+  })
+
+  return ( 
+    loaded ? 
+      <Card>
+        <CardActionArea>
+          <CardMedia className={classes.lazyImage} component='img' image={props.imageData} title='My House' />
+          <CardContent className={classes.block}>
+            <Typography variant='h5' component='h2'>
+              {props.street} 
+            </Typography>
+
+            <h4>Value</h4>
+            <Typography variant='body1' component='p'>
+              $ {props.value}
+            </Typography>
+
+            <h4>Description/Details</h4>
+            <Typography variant='body2' component='p'>
+              {props.description} 
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
-    </Paper>
-  );
+    : 
+      <Card> 
+        <CardActionArea>
+          <CardMedia className={classes.lazyImage} title='My House'><CircularProgress className={'mt-4'}/></CardMedia>
+           <CardContent>
+            <LinearProgress />
+            <h4>Value</h4>
+            <LinearProgress color='secondary'/>
+            <h4>Description/Details</h4>
+            <LinearProgress />
+          </CardContent>
+        </CardActionArea>
+      </Card>
+  )
 }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
-import moment from 'moment';
+import { DB } from '../api/firestore';
+import { AuthContext } from '../providers/AuthProvider';
+import { zillow } from '../api/zillow';
+import moment from 'moment'; //for fake data, can be removed or used elsewhere when the fake data is pulled out
+
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import CompList from '../components/Dashboard/CompList';
 import CompListItem from '../components/Dashboard/CompListItem';
@@ -11,19 +16,11 @@ import MyHouse from '../components/Dashboard/MyHouse';
 import FormChart from '../components/Dashboard/FormChart';
 import TrendingChart from '../components/Dashboard/TrendingChart';
 
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import '../App.css';
-import { DB } from '../api/firestore';
-import { AuthContext } from '../providers/AuthProvider';
-import { zillow } from '../api/zillow';
 
 function Home(props) {
-  // const { isAuth, logout } = useContext(AuthContext);
-
-  useEffect(() => {
-    fetchaddress();
-  }, []);
+  
   // TODO THIS DATA WILL BE COMING FROMTHE DB
   const formData = [
     { country: 'Russia', area: 12 },
@@ -46,7 +43,6 @@ function Home(props) {
   ];
 
   // House Display Info Logic - STEFFI
-
   const { user } = useContext(AuthContext);
   const [imageData, setImage] = useState([]);
   const [streetdisplay, setstreetdisplay] = useState('');
@@ -56,8 +52,10 @@ function Home(props) {
   const [compestatedisplay, setcompstatedisplay] = useState('');
   const [complastsoldprice, setcomplastsoldprice] = useState('');
   const [complastsolddate, setcomplastsolddate] = useState('');
+  const [value, setVale] = useState('');
+  const [description, setDescription] = useState('');
+  
   // House EVAL
-
   const finishedSqFt = '2466';
   let avgSqFt = 0;
   let avgPerSqFt = 0;
@@ -144,6 +142,10 @@ function Home(props) {
     }, 3000);
   };
 
+  useEffect(() => {
+    fetchaddress();
+  }, []);
+
   return (
     <Container className='signup'>
       <Grid container spacing={2} style={{ padding: 24 }}>
@@ -152,7 +154,12 @@ function Home(props) {
           <Typography variant='h4' component='h2'>
             My House
           </Typography>
-          <MyHouse street={streetdisplay} />
+          <MyHouse 
+            street={streetdisplay}
+            imageData={imageData}
+            value={value}
+            description={description}
+            />
         </Grid>
 
         {/* --------------- COMPS --------------- */}
