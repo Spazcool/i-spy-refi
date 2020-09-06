773e85d17a0483a2331ec7e633f852ff168a9ef2
Keith Beaudoin <bowdwin.muninted@gmail.com>
2020-09-06 11:05:33 -0400

    have basic template functioning

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,19 @@ import {
   BrowserRouter as Router,
   Redirect,
 } from 'react-router-dom';
+
 import { AuthProvider, AuthContext } from './providers/AuthProvider';
 import { CustomThemeProvider } from './providers/ThemeProvider';
 
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
 import ReactGA from 'react-ga';
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views
+import Home from './views/Home';
 
 import Dashboard from './pages/Dashboard';
 import Signup from './pages/Signup';
@@ -27,8 +33,14 @@ import SimilarProperties from './components/SimilarProperties';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-function App() {
+const trackPage = (page) => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
   const { isAuth } = useContext(AuthContext);
+
   const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
       {...rest}
@@ -37,33 +49,46 @@ function App() {
       }
     />
   );
-
   return (
-    <Router>
-      <>
-        <Navbar className='navbar' />
-        <Switch>
-          <Route exact path='/' render={(props) => <Splash {...props} />} />
-          <Route exact path='/login' render={(props) => <Login {...props} />} />
-          <Route
-            exact
-            path='/signup'
-            render={(props) => <Signup {...props} />}
-          />
-          {/* <PrivateRoute exact path='/houses' component={Houses} /> */}
-          <PrivateRoute
-            exact
-            path='/houseadditions'
-            component={HouseAdditions}
-          />
-          {/* <Route exact path='/signup' render={(props) => <Signup {...props} />} /> */}
-          <PrivateRoute exact path='/dashboard' component={Dashboard} />
-          <PrivateRoute exact path='/user' component={User} />
-        </Switch>
-      </>
-    </Router>
+    <ScrollReveal
+      children={() => (
+        <Router>
+          <>
+            {/* <Navbar className='navbar' /> // TODO */}
+            <Switch>
+              <AppRoute
+                exact
+                path='/'
+                component={Home}
+                layout={LayoutDefault}
+              />
+              {/* <Route exact path='/' render={(props) => <Splash {...props} />} /> */}
+              <Route
+                exact
+                path='/login'
+                render={(props) => <Login {...props} />}
+              />
+              <Route
+                exact
+                path='/signup'
+                render={(props) => <Signup {...props} />}
+              />
+              {/* <PrivateRoute exact path='/houses' component={Houses} /> */}
+              <PrivateRoute
+                exact
+                path='/houseadditions'
+                component={HouseAdditions}
+              />
+              {/* <Route exact path='/signup' render={(props) => <Signup {...props} />} /> */}
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/user' component={User} />
+            </Switch>
+          </>
+        </Router>
+      )}
+    />
   );
-}
+};
 
 export default () => {
   return (
